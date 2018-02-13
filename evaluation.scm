6d5bce1fa2fc79b7031335e582e7238cb16393c1
Seth E. Ruiz <ruizseth17@cs.grinnell.edu>
2018-02-13 13:50:23 -0600

    eval started

--- evaluation.scm
+++ evaluation.scm
@@ -2,10 +2,32 @@
         (provide best-mancala-eval)
         (require "mancala.scm") ; add any other modules you may need
 
-        
+;; Sourced from provided code in mancala-player.scm 
+;; Procedure
+;;   simple-mancala-eval
+;;
+;; Purpose
+;;   Create a procedure for evaluating a player's mancala state
+;;
+;; Parameters
+;;   player, a boolean
+;;
+;; Produces
+;;   eval-fun, a procedure
+;;
+;; Preconditions
+;;   [None]
+;;
+;; Postconditions
+;;   eval-fun takes a state and returns the number of stones in player's mancala      
 (define best-mancala-eval
-  (lambda (player)
+   (lambda (player)
     (lambda (state)
-      0)))
+      (let ((board (cdr state)))
+	(- (list-ref board (if player 6 13)) (list-ref board (if player 13 6)))))))
+
 
 ) ; module
+
+;; 
+
